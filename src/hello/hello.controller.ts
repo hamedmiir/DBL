8dbb01f9b1cefe07ca29ccabc5fa659cc4d3ee57
Hamed <hamedmiirz@gmail.com>
2020-12-14 19:09:13 +0330

    Add swagger to project

--- src/hello/hello.controller.ts
+++ src/hello/hello.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Post, Body, Get, Header, Query } from '@nestjs/common';
 import { HelloService } from './hello.service';
 import { PersonDto } from './dto/person.dto';
+import { ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
 
 @Controller('hello')
 export class HelloController {
@@ -10,11 +11,24 @@ export class HelloController {
 
   @Post('welcome')
   @Header('Content-Type', 'application/json')
+  @ApiResponse({status: 200, description: 'Say Hello!'})
   async sayWelcome(@Body() personDto: PersonDto): Promise<{data: String}> {
     let msg = await this.helloService.welcome(personDto);
     return {data : msg};
   }
 
+  @ApiResponse({status: 200})
+  @ApiQuery({
+    name: 'name',
+    required: true,
+    type: String
+  })
+  @ApiQuery({
+    name: 'year',
+    required: false,
+    type: Number,
+    description: 'You can ignore this'
+  })
   @Get('welcome')
   async sayWelcome2(@Query('name') iName, @Query('year') iYear): Promise<{data : String}>{
     let msg = await this.helloService.welcome({name: iName, year: iYear});
