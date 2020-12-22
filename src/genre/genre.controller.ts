8b3d5423cb64f81a8f555658737b79df6137c2c9
Hamed <hamedmiirz@gmail.com>
2020-12-22 20:11:15 +0330

    Add Update and Delete APIs to genres

--- src/genre/genre.controller.ts
+++ src/genre/genre.controller.ts
@@ -1,7 +1,7 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, ParseIntPipe, Post, Put, Query } from '@nestjs/common';
 import GenreServices from './genre.service';
 import CreateGenreDto from './dto/create-genre.dto';
-import { ApiResponse } from '@nestjs/swagger';
+import { ApiQuery, ApiResponse } from '@nestjs/swagger';
 
 @Controller('genre')
 export default class GenreController {
@@ -17,5 +17,27 @@ export default class GenreController {
   @Get()
   getAll() {
     return this.genreServices.getAllGenre();
+  }
+
+  @ApiResponse({status: 200, description: 'Delete a genre'})
+  @ApiQuery({
+    name: 'genreID',
+    required: true,
+    type: Number
+  })
+  @Delete()
+  deleteBook(@Query('genreID', ParseIntPipe) genreID: number) {
+    return this.genreServices.delete(genreID)
+  }
+
+  @ApiResponse({status: 200, description: 'Modify a genre'})
+  @ApiQuery({
+    name: 'genreID',
+    required: true,
+    type: Number
+  })
+  @Put()
+  modifyBook(@Query('genreID', ParseIntPipe) genreID: number, @Body() genre: CreateGenreDto) {
+    return this.genreServices.modify(genreID, genre);
   }
 }
--- src/genre/genre.service.ts
+++ src/genre/genre.service.ts
@@ -16,4 +16,18 @@ export default class GenreServices {
   async getAllGenre(): Promise<GenreEntity[]> {
     return await GenreEntity.find();
   }
+
+  async delete(genreID : number): Promise<GenreEntity> {
+    const genre : GenreEntity = await GenreEntity.findOne({where : {id: genreID}});
+    await GenreEntity.delete(genreID);
+    return genre;
+  }
+
+  async modify(genreID: number, genreDetails: CreateGenreDto) : Promise<GenreEntity> {
+    const existingGenre : GenreEntity = await GenreEntity.findOne({id: genreID});
+    if (genreDetails.type)
+      existingGenre.type = genreDetails.type;
+    await existingGenre.save();
+    return existingGenre;
+  }
 }
